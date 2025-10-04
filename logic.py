c4136ffb2ed1a6b6e3ed5378bebc593dddde65a8
WilliamSlamat136 <138879529+WilliamSlamat136@users.noreply.github.com>
2025-10-04 17:10:06 +0700

    Add files via upload

---
+++ logic.py
@@ -0,0 +1,55 @@
+import aiohttp  # A library for asynchronous HTTP requests
+import random
+
+class Pokemon:
+    pokemons = {}
+    # Object initialisation (constructor)
+    def __init__(self, pokemon_trainer):
+        self.pokemon_trainer = pokemon_trainer
+        self.pokemon_number = random.randint(1, 1000)
+        self.name = None
+        self.ability = None
+        if pokemon_trainer not in Pokemon.pokemons:
+            Pokemon.pokemons[pokemon_trainer] = self
+        else:
+            self = Pokemon.pokemons[pokemon_trainer]
+
+    async def get_name(self):
+        # An asynchronous method to get the name of a pokémon via PokeAPI
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # URL API for the request
+        async with aiohttp.ClientSession() as session:  # Opening an HTTP session
+            async with session.get(url) as response:  # Sending a GET request
+                if response.status == 200:
+                    data = await response.json()  # Receiving and decoding JSON response
+                    return data['forms'][0]['name']  # Returning a Pokémon's name
+                else:
+                    return "Pikachu"  # Return the default name if the request fails
+
+    async def info(self):
+        # A method that returns information about the pokémon
+        if not self.name:
+            self.name = await self.get_name()  # Retrieving a name if it has not yet been uploaded
+            await self.get_ability()
+        return f"The name of your Pokémon: {self.name} \nability {self.ability}"  # Returning the string with the Pokémon's name
+
+    async def show_img(self):
+        # An asynchronous method to retrieve the URL of a pokémon image via PokeAPI\
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # URL API for the request
+        async with aiohttp.ClientSession() as session:  # Opening an HTTP session
+            async with session.get(url) as response:  # Sending a GET request
+                if response.status == 200:
+                    data = await response.json()  # Receiving and decoding JSON response
+                    return data['sprites']["other"]['showdown']["back_default"]  # Returning a Pokémon's name
+                else:
+                    return "Pikachu"  # Return the default name if the request fails
+
+    async def get_ability(self):
+        # An asynchronous method to retrieve the URL of a pokémon image via PokeAPI\
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # URL API for the request
+        async with aiohttp.ClientSession() as session:  # Opening an HTTP session
+            async with session.get(url) as response:  # Sending a GET request
+                if response.status == 200:
+                    data = await response.json()  # Receiving and decoding JSON response
+                    return data['abilities'][0]['ability']["name"]  # Returning a Pokémon's name
+                else:
+                    return "Pikachu"  # Return the default name if the request fails
