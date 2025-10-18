cac608b412e5f02d892017737ce45bc83d70fc83
WilliamSlamat136 <138879529+WilliamSlamat136@users.noreply.github.com>
2025-10-18 16:43:40 +0700

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,6 +1,6 @@
 import aiohttp  # A library for asynchronous HTTP requests
 import random
-import asyncio
+from datetime import datetime, timedelta
 
 class Pokemon:
     pokemons = {}
@@ -10,9 +10,6 @@ class Pokemon:
         self.pokemon_number = random.randint(1, 1000)
         self.name = None
         self.ability = None
-        self.hp = random.randint(10, 30)
-        self.power = random.randint(1, 5)
-
         if pokemon_trainer not in Pokemon.pokemons:
             Pokemon.pokemons[pokemon_trainer] = self
         else:
@@ -28,70 +25,65 @@ class Pokemon:
                     return data['forms'][0]['name']  # Returning a Pokémon's name
                 else:
                     return "Pikachu"  # Return the default name if the request fails
-
+    
     async def info(self):
         # A method that returns information about the pokémon
         if not self.name:
-            self.name = await self.get_name()  # Retrieving a name if it has not yet been uploaded
-            await self.get_ability()
-        return f"The name of your Pokémon: {self.name} \nability {self.ability} \nhp: {self.hp} \npower: {self.power}"  # Returning the string with the Pokémon's name
+            self.name = await self.get_name() 
+        self.ability = await self.get_ability() # Retrieving a name if it has not yet been uploaded
+        return f"The name of your Pokémon: {self.name} ability {self.ability}"  # Returning the string with the Pokémon's name
 
     async def show_img(self):
-        # An asynchronous method to retrieve the URL of a pokémon image via PokeAPI\
+        # An asynchronous method to retrieve the URL of a pokémon image via PokeAPI
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # URL API for the request
         async with aiohttp.ClientSession() as session:  # Opening an HTTP session
             async with session.get(url) as response:  # Sending a GET request
                 if response.status == 200:
                     data = await response.json()  # Receiving and decoding JSON response
-                    return data['sprites']["other"]['showdown']["back_default"]  # Returning a Pokémon's name
+                    return data['sprites']['front_default']  # Returning a Pokémon's name
                 else:
-                    return "Pikachu"  # Return the default name if the request fails
-
-    async def get_ability(self):
-        # An asynchronous method to retrieve the URL of a pokémon image via PokeAPI\
-        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # URL API for the request
-        async with aiohttp.ClientSession() as session:  # Opening an HTTP session
-            async with session.get(url) as response:  # Sending a GET request
-                if response.status == 200:
-                    data = await response.json()  # Receiving and decoding JSON response
-                    return data['abilities'][0]['ability']["name"]  # Returning a Pokémon's name
-                else:
-                    return "Pikachu"  # Return the default name if the request fails
+                    return "No image"  # Return the default name if the request fails
                 
-    async def attack(self, enemy):
-        if isinstance(enemy, Wizard):  # Periksa apakah musuh adalah tipe data Penyihir (instance dari kelas Penyihir)
-            kesempatan = random.randint(1,5)
-            if kesempatan == 1:
-                return "Pokemon penyihir menggunakan perisai dalam pertarungan"
-            
-        if enemy.hp > self.power:
-            enemy.hp -= self.power
-            return f"Pertarungan @{self.pokemon_trainer} dengan @{enemy.pokemon_trainer}"
+    async def get_ability(self):
+        # An asynchronous method to retrieve the URL of a pokémon image via PokeAPI
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # URL API for the request
+        async with aiohttp.ClientSession() as session:  # Opening an HTTP session
+            async with session.get(url) as response:  # Sending a GET request
+                if response.status == 200:
+                    data = await response.json()  # Receiving and decoding JSON response
+                    return data['abilities'][0]['ability']['name']  # Returning a Pokémon's name
+                else:
+                    return "No ability"  # Return the default name if the request fails
+
+    async def feed(self, feed_interval = 20, hp_increase = 10 ):
+        current_time = datetime.now()  
+        delta_time = timedelta(hours=feed_interval)  
+        if (current_time - self.last_feed_time) > delta_time:
+            self.hp += hp_increase
+            self.last_feed_time = datetime.now()
+            return f"Kesehatan Pokemon dipulihkan. HP saat ini: {self.hp}"
         else:
-            enemy.hp = 0
-            return f"@{self.pokemon_trainer} menang melawan @{enemy.pokemon_trainer}!"
+            return f"Kalian dapat memberi makan Pokémon kalian di: {current_time-delta_time}"
         
+
 class Wizard(Pokemon):
-    async def attack(self, enemy):
-        return await super().attack(enemy)
-    
+    async def feed(self, feed_interval = 10, hp_increase = 10 ):
+        current_time = datetime.now()  
+        delta_time = timedelta(hours=feed_interval)  
+        if (current_time - self.last_feed_time) > delta_time:
+            self.hp += hp_increase
+            self.last_feed_time = datetime.now()
+            return f"Kesehatan Pokemon dipulihkan. HP saat ini: {self.hp}"
+        else:
+            return f"Kalian dapat memberi makan Pokémon kalian di: {current_time-delta_time}"
+        
 class Fighter(Pokemon):
-    async def attack(self, enemy):
-        kekuatan_super = random.randint(5,15)
-        self.power += kekuatan_super
-        hasil = await super().attack(enemy)
-        self.power -= kekuatan_super
-        return hasil + f"\nPetarung menggunakan serangan super dengan kekuatan:{kekuatan_super} "
-    
-async def main():
-    wizard = Wizard("username1")
-    fighter = Fighter("username2")
-
-    print(await wizard.info())
-    print()
-    print(await fighter.info())
-    print()
-    print(await fighter.attack(wizard)) 
-
-if __name__ == '__main__':
-    asyncio.run(main())
+    async def feed(self, feed_interval = 20, hp_increase = 10 ):
+        current_time = datetime.now()  
+        delta_time = timedelta(hours=feed_interval)  
+        if (current_time - self.last_feed_time) > delta_time:
+            self.hp += hp_increase
+            self.last_feed_time = datetime.now()
+            return f"Kesehatan Pokemon dipulihkan. HP saat ini: {self.hp}"
+        else:
+            return f"Kalian dapat memberi makan Pokémon kalian di: {current_time-delta_time}"
